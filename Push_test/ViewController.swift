76669558d18174a3ebf409687b21859451e4c607
Akos Birmacher <akos.birmacher@bitrise.io>
2019-03-29 14:50:32 +0100

    init

---
+++ Push_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Push_test
+//
+//  Created by Akos Birmacher on 2019. 03. 29..
+//  Copyright © 2019. Bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
